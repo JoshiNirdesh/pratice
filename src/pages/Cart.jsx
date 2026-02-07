1a2bbea6c0271922e3386e5cc70c541f94057703
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-02-07 18:29:52 +0545

    cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,47 +1,20 @@
-import React, { useState } from "react";
-import Navbar from "../components/Navbar";
+
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Cart = () => {
-    const [cartItems,setCartItems]=useState([])
-    const addToCart = (itemId)=>{
-        let cartData = {...cartItems};
-        if(cartData[itemId]){
-            cartData[itemId]+=1;
-        }
-        else{
-            cartData[itemId]=1
-        }
-        setCartItems(cartData)
-        console.log(cartData)
+    const [cartArray,setCartArray]=useState([]);
+    const {cartItems}=useContext(AppContext);
+
+    const getCart = ()=>{
+        
     }
-    const getCartCount = ()=>{
-        let totalCount  = 0;
-        for(const items in cartItems){
-            totalCount+=cartItems[items]
-        }
-        return totalCount
-    } 
-    const removeFromCart=(itemId)=>{
-        let cartData = {...cartItems}
-        if(cartData[itemId]){
-            cartData[itemId]-=1
-        }
-        if(cartData[itemId]===0){
-            delete cartData[itemId]
-        }
-        setCartItems(cartData)
-    }
+
   return (
     <div>
-      <Navbar />
-      <div onClick={()=>addToCart(2)}>Add</div>
-            <div onClick={()=>addToCart(3)}>Add</div>
-                        <div onClick={()=>removeFromCart(3)}>Remove</div>
-
-            <div>{getCartCount()}</div>
-
+      
     </div>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
