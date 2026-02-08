d5729a1992217104ce7d934b041599ecd5c8d25d
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-02-08 18:42:39 +0545

    Json token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,6 @@
 const userModel = require("../model/userModel");
+const bcrypt = require("bcrypt")
+const JWT = require("jsonwebtoken")
 
 const registerController = async (req, res) => {
   const { username, email, password } = req.body;
@@ -16,7 +18,9 @@ const registerController = async (req, res) => {
         message: "User Already Exist",
       });
     }
-    const newUser = await userModel.create({ username, email, password });
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password,salt)
+    const newUser = await userModel.create({ username, email, password:hashPassword});
     res.status(400).send({
         success:true,
         message:"Register Successfully",
@@ -31,4 +35,45 @@ const registerController = async (req, res) => {
   }
 };
 
-module.exports = registerController;
+const loginController = async (req,res)=>{
+    const {email,password} = req.body
+
+    try {
+        if(!email || !password){
+            return res.status(500).send({
+                success:false,
+                message:"All fields are required"
+            })
+        }
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(500).send({
+                success:false,
+                message:"Invalid Credentials "
+            })
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(500).send({
+                success:false,
+                message:"Invalid Credentials"
+            })
+        }
+        const token = JWT.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"})
+        res.status(200).send({
+            success:true,
+            message:"Login Successfully",
+            token,
+            user
+        })
+    } catch (error) {
+        res.status(500).send({
+            success:false,
+            message:"Login Api error",
+            error:error.message
+        })
+    }
+
+}
+
+module.exports = {registerController,loginController};
