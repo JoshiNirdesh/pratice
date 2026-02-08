eed753b00e0cb407bb157d88dbe7bb29ea97c181
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-02-08 12:40:58 +0545

    backend port

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import React, { useContext } from "react";
-import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
-import Category from "./pages/Category";
-import ProductDetails from "./components/ProductDetails";
-import AllProduct from "./pages/AllProduct";
-import Cart from "./pages/Cart";
-import { AppContext } from "./context/AppContext";
-import Login from "./components/Login";
+import React from 'react'
 
 const App = () => {
-  const {showUserLogin}=useContext(AppContext)
   return (
-    <div className="mr-15 ml-15">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/categories/:category" element={<Category />}></Route>
-        <Route path="/product/:id" element={<ProductDetails />}></Route>
-        <Route path="/product" element={<AllProduct />}></Route>
-        <Route path="/cart" element={<Cart/>}></Route>
-      </Routes>
-              {showUserLogin ? <Login/>:""}
-
+    <div>
+      This is app
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
