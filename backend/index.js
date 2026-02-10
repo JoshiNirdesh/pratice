2d2f2ad4557f94369036e6516e467e4a77fe1efc
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-02-10 09:52:16 +0545

    basic server

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/dbconfig");
 
-dotenv.config();
 const app = express();
-app.use(express.json());
-connectDB();
-const port = process.env.PORT;
 
-app.use("/api/v1/", require("./router/userRoutes"));
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+app.get("/",(req,res)=>{
+    res.send("This is home")
+})
+app.listen(4000,()=>{
+    console.log("Listening on port 4000");
+})
