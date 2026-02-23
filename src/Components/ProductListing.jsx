d0ce05aa95db705bf5fea6e4c6c6350fbbf8e103
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-02-23 11:53:58 +0545

    product price sort

--- src/Components/ProductListing.jsx
+++ src/Components/ProductListing.jsx
@@ -4,6 +4,7 @@ import ProductCard from "./ProductCard";
 const ProductListing = () => {
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState("all");
+  const [sort, setSort] = useState("asc");
 
   const fetchProduct = async () => {
     const res = await fetch("https://fakestoreapi.com/products");
@@ -20,6 +21,10 @@ const ProductListing = () => {
     category == "all"
       ? products
       : products.filter((products) => products.category === category);
+
+  const priceSort = [...filterProducts].sort((a, b) =>
+    sort === "asc" ? a.price - b.price : b.price - a.price,
+  );
   return (
     <>
       <div>
@@ -37,9 +42,18 @@ const ProductListing = () => {
           <option value="electronics">Electronics</option>
           <option value="women's clothing">Women's Clothing</option>
         </select>
+        <p>Sort by Price</p>
+        <select
+          className="border ml-2"
+          value={sort}
+          onChange={(e) => setSort(e.target.value)}
+        >
+          <option value="asc">Low → High</option>
+          <option value="desc">High → Low</option>
+        </select>
       </div>
       <div className="grid grid-cols-5 px-12 py-10">
-        {filterProducts.map((product, index) => (
+        {priceSort.map((product, index) => (
           <ProductCard product={product} key={index} />
         ))}
       </div>
